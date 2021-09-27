148766913a81755ffbf30494eba1e8f3e03d781b
bhavya <bhavyaala001@gmail.com>
2021-09-27 17:30:07 +0000

    Added search features in file operations

--- src/project2/Projectmain.java
+++ src/project2/Projectmain.java
@@ -1,8 +1,8 @@
 package project2;
+//import java.util.ArrayList;
+//import java.util.Collections;
+//import java.util.List;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Projectmain {
 
@@ -18,44 +18,67 @@ public class Projectmain {
 					Scanner sc = new Scanner(System.in);
 					
 					do {
-						Menuoptions.displayMenu();
-						int input = sc.nextInt();
+					//	MenuOptions.displayMenu();
+						//int input = sc.nextInt();
+						try {
+							Menuoptions.displayMenu();
+							int input = sc.nextInt();
 
-						switch (input) {
-						case 1:
-							// All required files and folders inside main folder relative to current folder
-							//Fileoperations.listFilesInDirectory("./main", 0);
-							displayAllFiles();
-							break;
-						case 2:
-							Menuoptions.displayFileMenuOptions();
-							break;
-						case 3:
-							System.out.println("Program exited successfully.");
-							System.exit(0);
-							break;
-						default:
-							System.out.println("Please select a valid option from above.");
-						}
+						//switch (input) {
+						//case 1:
+							//displayAllFiles();
+							//break;
+						//case 2:
+							//MenuOptions.displayFileMenuOptions();
+							//break;
+
+						//case 3:
+							//System.out.println("Program exited successfully.");
+							//System.exit(0);
+							//break;
+						//default:
+							//System.out.println("Please select a valid option from above.");
+					//	}
+							switch (input) {
+							case 1:
+								Fileoperations.displayAllFiles("./main");
+								break;
+							case 2:
+								Menuoptions.handleFileMenuOptions();
+								break;
+							case 3:
+								System.out.println("Program exited successfully.");
+								running = false;
+								sc.close();
+								System.exit(0);
+								break;
+							default:
+								System.out.println("Please select a valid option from above.");
+							}
+						} catch (Exception e) {
+							System.out.println(e.getClass().getName());
+							handleWelcomeScreenInput();
+						} 
 					} while (running == true);
-					
-					sc.close();
+
+					//sc.close();
 
 				}
 
-				private static void displayAllFiles() {
+				//private static void displayAllFiles() {
 					// All required files and folders inside main folder relative to current folder
-					System.out.println("Displaying all files with directory structure in ascending order\n");
-					List<String> filesListNames = Fileoperations.listFilesInDirectory("./main", 0, new ArrayList<String>());
+					//System.out.println("Displaying all files with directory structure in ascending order\n");
+					//List<String> filesListNames = Fileoperations.listFilesInDirectory("./main", 0, new ArrayList<String>());
 
-					System.out.println("Displaying all files in ascending order\n");
-					Collections.sort(filesListNames);
+					//System.out.println("Displaying all files in ascending order\n");
+					//Collections.sort(filesListNames);
 
-					filesListNames.stream()
-							.forEach(System.out::println);
+					//filesListNames.stream()
+							//.forEach(System.out::println);
 				}
 			
-}
+			
+
 
 			 
 			
