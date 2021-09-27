916e6e7670bbd8ea13e9f0b9e46b096975b9f443
bhavya <bhavyaala001@gmail.com>
2021-09-27 15:39:24 +0000

    Add options to display all files

--- src/project2/Fileoperations.java
+++ src/project2/Fileoperations.java
@@ -1,30 +1,47 @@
 package project2;
 import java.io.File;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class Fileoperations {
-	public static void listFilesInDirectory(String path, int indentationCount) {
-		File dir = new File(path);
-        File[] files = dir.listFiles();
+	//public static void listFilesInDirectory(String path, int indentationCount) {
+		 
+			public static List<String> listFilesInDirectory(String path, int indentationCount, List<String> fileListNames) {
+				File dir = new File(path);
+		        File[] files = dir.listFiles();
 
-        if (files != null && files.length > 0) {
-            for (File file : files) {
+		        List<File> filesList = Arrays.asList(files);
 
-            	System.out.print(" ". repeat(indentationCount*2));
+		        Collections.sort(filesList);
+
+		        if (files != null && files.length > 0) {
+		            //for (File file : files) {
+		            for (File file : filesList) {
+
+		            	System.out.print(" " +(indentationCount*2));
+
+		                
+		            	if (file.isDirectory()) {  
+		            		System.out.println("`-- " + file.getName());
+
+		            		// Recursively indent and display the files
+		                	//listFilesInDirectory(file.getAbsolutePath(), indentationCount+1);
+		            		fileListNames.add(file.getName());
+		                	listFilesInDirectory(file.getAbsolutePath(), indentationCount+1, fileListNames);
+		                }  else {
+		                	System.out.println("|-- " + file.getName());
+		                	fileListNames.add(file.getName());
+		                }
+		            }
+		        } else {
+		        	System.out.print(" " +(indentationCount*2));
+		        	System.out.println("|-- Empty Directory");
+		        }
+		        System.out.println();
+		        return fileListNames;
+			}
 
 
-            	if (file.isDirectory()) {  
-            		System.out.println("`-- " + file.getName());
+		}
 
-            		// Recursively indent and display the files
-                	listFilesInDirectory(file.getAbsolutePath(), indentationCount+1);
-                }  else {
-                	System.out.println("|-- " + file.getName());
-                }
-            }
-        } else {
-        	System.out.print(" ".repeat(indentationCount*2));
-        	System.out.println("|-- Empty Directory");
-        }
-        System.out.println();
-	}
-}
--- src/project2/Projectmain.java
+++ src/project2/Projectmain.java
@@ -1,9 +1,64 @@
 package project2;
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Projectmain {
 
 	public static void main(String[] args) {
 		Welcome1.printWelcome1("Welcome1", "Bhavya");
+		
+				
+				Menuoptions.printWelcome1("Welcome1", "Bhavya");
+				handleWelcomeScreenInput();
 	}
+				private static void handleWelcomeScreenInput() {
+					boolean running = true;
+					Scanner sc = new Scanner(System.in);
+					
+					do {
+						Menuoptions.displayMenu();
+						int input = sc.nextInt();
 
+						switch (input) {
+						case 1:
+							// All required files and folders inside main folder relative to current folder
+							//Fileoperations.listFilesInDirectory("./main", 0);
+							displayAllFiles();
+							break;
+						case 2:
+							Menuoptions.displayFileMenuOptions();
+							break;
+						case 3:
+							System.out.println("Program exited successfully.");
+							System.exit(0);
+							break;
+						default:
+							System.out.println("Please select a valid option from above.");
+						}
+					} while (running == true);
+					
+					sc.close();
+
+				}
+
+				private static void displayAllFiles() {
+					// All required files and folders inside main folder relative to current folder
+					System.out.println("Displaying all files with directory structure in ascending order\n");
+					List<String> filesListNames = Fileoperations.listFilesInDirectory("./main", 0, new ArrayList<String>());
+
+					System.out.println("Displaying all files in ascending order\n");
+					Collections.sort(filesListNames);
+
+					filesListNames.stream()
+							.forEach(System.out::println);
+				}
+			
 }
+
+			 
+			
+		
+	
+
