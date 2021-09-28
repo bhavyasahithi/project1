18c5743db0d8e916a51119a446b10f31aef0ff09
bhavya <bhavyaala001@gmail.com>
2021-09-28 06:09:08 +0000

    Added options to create main folder if doesn't exists

--- src/project2/Menuoptions.java
+++ src/project2/Menuoptions.java
@@ -31,23 +31,26 @@ public class Menuoptions {
 		do {
 			try {
 				displayFileMenuOptions();
+				Fileoperations.createMainFolderIfNotPresent("main");
+
 				int input = sc.nextInt();
 				switch (input) {
 				case 1:
 					// File Add
 					System.out.println("Enter the name of the file to be added to the \"main\" folder");
 					String fileToAdd = sc.next();
-
+					
 					Fileoperations.createFile(fileToAdd, sc);
-
+					
 					break;
 				case 2:
 					// File/Folder delete
 					System.out.println("Enter the name of the file to be deleted from \"main\" folder");
 					String fileToDelete = sc.next();
 
+					Fileoperations.createMainFolderIfNotPresent("main");
 					List<String> filesToDelete = Fileoperations.displayFileLocations(fileToDelete, "main");
-					
+
 					String deletionPrompt = "\nSelect index of which file to delete?"
 							+ "\n(Enter 0 if you want to delete all elements)";
 					System.out.println(deletionPrompt);
@@ -64,12 +67,13 @@ public class Menuoptions {
 						}
 					}
 					
-
 					break;
 				case 3:
 					// File/Folder Search
 					System.out.println("Enter the name of the file to be searched from \"main\" folder");
 					String fileName = sc.next();
+
+					Fileoperations.createMainFolderIfNotPresent("main");
 					Fileoperations.displayFileLocations(fileName, "main");
 
 
@@ -92,7 +96,10 @@ public class Menuoptions {
 			}
 		} while (running == true);
 	}
-}
+
+									
+	}
+
 	
 		
 	
--- src/project2/Projectmain.java
+++ src/project2/Projectmain.java
@@ -6,39 +6,46 @@ import java.util.Scanner;
 
 public class Projectmain {
 	public static void main(String[] args) {
-		Menuoptions.printWelcome1("Welcome1" , "Bhavya");
-		handleWelcomeScreenInput();
-	}
-	private static void handleWelcomeScreenInput() {
-		boolean running = true;
-		Scanner sc = new Scanner(System.in);
-		do {
-			try {
-				Menuoptions.displayMenu();
-				int input = sc.nextInt();
+		// Create "main" folder if not present in current folder structure
+				Fileoperations.createMainFolderIfNotPresent("main");
 
-				switch (input) {
-				case 1:
-					//Fileoperations.displayAllFiles("./main");
-					Fileoperations.displayAllFiles("main");
-					break;
-				case 2:
-					Menuoptions.handleFileMenuOptions();
-					break;
-				case 3:
-					System.out.println("Program exited successfully.");
-					running = false;
-					sc.close();
-					System.exit(0);
-					break;
-				default:
-					System.out.println("Please select a valid option from above.");
-				}
-			} catch (Exception e) {
-				System.out.println(e.getClass().getName());
+				Menuoptions.printWelcome1("Welcome1", "Bhavya");
+
 				handleWelcomeScreenInput();
-			} 
-		} while (running == true);
+			}
+
+			private static void handleWelcomeScreenInput() {
+				boolean running = true;
+				Scanner sc = new Scanner(System.in);
+				do {
+					try {
+						Menuoptions.displayMenu();
+						int input = sc.nextInt();
+						switch (input) {
+						case 1:
+							Fileoperations.displayAllFiles("main");
+							break;
+						case 2:
+							Menuoptions.handleFileMenuOptions();
+							break;
+						case 3:
+							System.out.println("Program exited successfully.");
+							running = false;
+							sc.close();
+							System.exit(0);
+							break;
+						default:
+							System.out.println("Please select a valid option from above.");
+						}
+					} catch (Exception e) {
+						System.out.println(e.getClass().getName());
+						handleWelcomeScreenInput();
+					} 
+				} while (running == true);
+			}
+		
+		
+		
 	}
-}
+
 		
